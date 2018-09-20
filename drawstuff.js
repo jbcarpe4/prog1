caac4205ea355af005d42d8cf8f665c2a0149ed4
Joshua Carpenter <joshua1575@gmail.com>
2018-09-20 17:50:14 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -230,6 +230,66 @@ function drawInputTrianglesUsingPaths(context) {
     } // end if triangle files found
 } // end draw input triangles
 
+function rayCastNoLighting(context) {
+    var inputTriangles = getInputTriangles();
+    var w = context.canvas.width;
+    var h = context.canvas.height;
+    var imagedata = context.createImageData(w,h);
+    const PIXEL_DENSITY = 0.1;
+    var numCanvasPixels = (w*h)*PIXEL_DENSITY; 
+    
+    if (inputTriangles != String.null) { 
+        var x = 0; var y = 0; // pixel coord init
+        var cx = 0; var cy = 0; // init center x and y coord
+        var numTrianglePixels = 0; // init num pixels in triangle
+        var c = new Color(0,0,0,0); // init the triangle color
+        var n = inputTriangles.length; // the number of input files
+
+        // Loop over the triangles, draw rand pixels in each
+        for (var f=0; f<n; f++) {
+        	var tn = inputTriangles[f].triangles.length;
+        	
+        	// Loop over the triangles, draw each in 2d
+        	for(var t=0; t<tn; t++){
+        		var vertex1 = inputTriangles[f].triangles[t][0];
+        		var vertex2 = inputTriangles[f].triangles[t][1];
+        		var vertex3 = inputTriangles[f].triangles[t][2];
+
+        		var vertexPos1 = inputTriangles[f].vertices[vertex1];
+        		var vertexPos2 = inputTriangles[f].vertices[vertex2];
+        		var vertexPos3 = inputTriangles[f].vertices[vertex3];
+        		
+        		// triangle position on canvas
+        		
+        		var v1 = [w*vertexPos1[0], h*vertexPos1[1]];
+        		var v2 = [w*vertexPos2[0], h*vertexPos2[1]];
+        		var v3 = [w*vertexPos3[0], h*vertexPos3[1]];
+        		
+        		// calculate triangle area on canvas (shoelace formula)
+        		var triangleArea = 0.5*Math.abs(v1[0]*v2[1]+v2[0]*v3[1]+v3[0]*v1[1]-v2[0]*v1[1]-v3[0]*v2[1]-v1[0]*v3[1]);
+        		var numTrianglePixels = triangleArea; // init num pixels in triangle
+
+                numTrianglePixels *= PIXEL_DENSITY; // percentage of triangle area to render to pixels
+            	numTrianglePixels = Math.round(numTrianglePixels);
+
+                c.change(
+            		inputTriangles[f].material.diffuse[0]*255,
+                	inputTriangles[f].material.diffuse[1]*255,
+                	inputTriangles[f].material.diffuse[2]*255,
+                	255); // triangle diffuse color
+            	for (var p=0; p<numTrianglePixels; p++) {
+                    var point = [w, h]; // on canvas plane
+            		var triangleTest = 0;
+
+            		drawPixel(imagedata,point[0],point[1],c);
+
+            	} // end for pixels in triangle
+        	} // end for triangles
+    	} // end for files
+        context.putImageData(imagedata, 0, 0);
+    } // end if triangle file found
+}
+
 function main() {
 
     // Get the canvas and context
