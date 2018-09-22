87763c0577213392b680c7485554ff4a611ba873
Joshua Carpenter <joshua1575@gmail.com>
2018-09-21 22:30:58 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -38,6 +38,144 @@ class Color {
     } // end Color change method
 } // end color class
 
+// Vector class
+class Vector { 
+    constructor(x,y,z) {
+        this.set(x,y,z);
+    } // end constructor
+    
+    // sets the components of a vector
+    set(x,y,z) {
+        try {
+            if ((typeof(x) !== "number") || (typeof(y) !== "number") || (typeof(z) !== "number"))
+                throw "vector component not a number";
+            else
+                this.x = x; this.y = y; this.z = z; 
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    } // end vector set
+    
+    // copy the passed vector into this one
+    copy(v) {
+        try {
+            if (!(v instanceof Vector))
+                throw "Vector.copy: non-vector parameter";
+            else
+                this.x = v.x; this.y = v.y; this.z = v.z;
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+        }
+    }
+    
+    toConsole(prefix="") {
+        console.log(prefix+"["+this.x+","+this.y+","+this.z+"]");
+    } // end to console
+    
+    // static dot method
+    static dot(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.dot: non-vector parameter";
+            else
+                return(v1.x*v2.x + v1.y*v2.y + v1.z*v2.z);
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(NaN);
+        }
+    } // end dot static method
+    
+    // static cross method
+    static cross(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.cross: non-vector parameter";
+            else {
+                var crossX = v1.y*v2.z - v1.z*v2.y;
+                var crossY = v1.z*v2.x - v1.x*v2.z;
+                var crossZ = v1.x*v2.y - v1.y*v2.x;
+                return(new Vector(crossX,crossY,crossZ));
+            } // endif vector params
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(NaN);
+        }
+    } // end dot static method
+    
+    // static add method
+    static add(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.add: non-vector parameter";
+            else
+                return(new Vector(v1.x+v2.x,v1.y+v2.y,v1.z+v2.z));
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end add static method
+
+    // static subtract method, v1-v2
+    static subtract(v1,v2) {
+        try {
+            if (!(v1 instanceof Vector) || !(v2 instanceof Vector))
+                throw "Vector.subtract: non-vector parameter";
+            else {
+                var v = new Vector(v1.x-v2.x,v1.y-v2.y,v1.z-v2.z);
+                return(v);
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end subtract static method
+
+    // static scale method
+    static scale(c,v) {
+        try {
+            if (!(typeof(c) === "number") || !(v instanceof Vector))
+                throw "Vector.scale: malformed parameter";
+            else
+                return(new Vector(c*v.x,c*v.y,c*v.z));
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end scale static method
+    
+    // static normalize method
+    static normalize(v) {
+        try {
+            if (!(v instanceof Vector))
+                throw "Vector.normalize: parameter not a vector";
+            else {
+                var lenDenom = 1/Math.sqrt(Vector.dot(v,v));
+                return(Vector.scale(lenDenom,v));
+            }
+        } // end try
+        
+        catch(e) {
+            console.log(e);
+            return(new Vector(NaN,NaN,NaN));
+        }
+    } // end scale static method
+    
+} // end Vector class
+
 
 /* utility functions */
 
@@ -236,7 +374,6 @@ function rayCastNoLighting(context) {
     var h = context.canvas.height;
     var eye = [w/2, h/2] //eye location at (0.5, 0.5)
     var imagedata = context.createImageData(w,h);
-    const PIXEL_DENSITY = 0.1;
     
     if (inputTriangles != String.null) { 
         var c = new Color(0,0,0,0); // init the triangle color
@@ -290,6 +427,83 @@ function rayCastNoLighting(context) {
     } // end if triangle file found
 }
 
+function rayCastBlinnPhong(context) {
+    var inputTriangles = getInputTriangles();
+    var w = context.canvas.width;
+    var h = context.canvas.height;
+    var eye = [w/2, h/2]
+    var imagedata = context.createImageData(w,h);
+    
+    if (inputTriangles != String.null) { 
+        var c = new Color(0,0,0,0); // init the triangle color
+        var n = inputTriangles.length; // the number of input files
+
+        // Loop over the triangles, draw rand pixels in each
+        for (var f=0; f<n; f++) {
+        	var tn = inputTriangles[f].triangles.length;
+        	
+        	// Loop over the triangles, draw each in 2d
+        	for(var t=0; t<tn; t++){
+        		var vertex1 = inputTriangles[f].triangles[t][0];
+        		var vertex2 = inputTriangles[f].triangles[t][1];
+        		var vertex3 = inputTriangles[f].triangles[t][2];
+
+        		var vertexPos1 = inputTriangles[f].vertices[vertex1];
+        		var vertexPos2 = inputTriangles[f].vertices[vertex2];
+        		var vertexPos3 = inputTriangles[f].vertices[vertex3];
+        		
+        		// triangle position on canvas
+        		var v1 = [w*vertexPos1[0], h*vertexPos1[1]];
+        		var v2 = [w*vertexPos2[0], h*vertexPos2[1]];
+        		var v3 = [w*vertexPos3[0], h*vertexPos3[1]];
+                                
+                var shade1 = inputTriangles[f].material.ambient[0] + inputTriangles[f].material.diffuse[0] +
+                             inputTriangles[f].material.specular[0];
+                var shade2 = inputTriangles[f].material.ambient[1] + inputTriangles[f].material.diffuse[1] +
+                             inputTriangles[f].material.specular[1];
+                var shade3 = inputTriangles[f].material.ambient[2] + inputTriangles[f].material.diffuse[2] +
+                             inputTriangles[f].material.specular[2];
+                
+                c.change(
+            		inputTriangles[f].material.diffuse[0]*255,
+                	inputTriangles[f].material.diffuse[1]*255,
+                	inputTriangles[f].material.diffuse[2]*255,
+                	255); // triangle diffuse color
+                
+                
+                for (var row = 0; row < w; row++) {
+                    for (var col = 0; col < h; col++) {                  
+            			var point = [row, col];
+                        
+                    	// plane checking            			
+                    	var t1 = ((point[0]-v2[0]) * (v1[1] - v2[1]) - (v1[0] - v2[0]) * (point[1] - v2[1])) < 0.0;
+                    	var t2 = ((point[0]-v3[0]) * (v2[1] - v3[1]) - (v2[0] - v3[0]) * (point[1] - v3[1])) < 0.0;
+                    	var t3 = ((point[0]-v1[0]) * (v3[1] - v1[1]) - (v3[0] - v1[0]) * (point[1] - v1[1])) < 0.0;
+                    	
+                        // draw the pixel if inside the triangle
+                    	if((t1==t2)&&(t2==t3)) {
+                            var pixPos = new Vector(row, col, vertexPos1[2] * w);
+                            var lVect = new Vector(-3 * w, w, -0.5 * w);
+                            lVect = Vector.subtract(lVect,worldLoc);
+                            lVect = Vector.normalize(lVect);
+                            var NdotL = Vector.dot(lVect,new Vector(0,0,1)); // rect in xy plane
+
+                            var difColor = new Color();
+                            // calc diffuse color
+                            difColor.r = inputTriangles[f].material.diffuse[0]*255 * NdotL;
+                            difColor.g = inputTriangles[f].material.diffuse[1]*255 * NdotL;
+                            difColor.b = inputTriangles[f].material.diffuse[2]*255 * NdotL;
+                            
+                    		drawPixel(imagedata, point[0], h - point[1], difColor);
+                        }
+            		}
+            	} // end for pixels in triangle
+        	} // end for triangles
+    	} // end for files
+        context.putImageData(imagedata, 0, 0);
+    } // end if triangle file found
+}
+
 function main() {
 
     // Get the canvas and context
@@ -305,5 +519,9 @@ function main() {
     //drawInputTrianglesUsingPaths(context);
     // shows how to read input file, but not how to draw pixels
     
-    rayCastNoLighting(context);
+    //rayCastNoLighting(context);
+    //part 1
+    
+    rayCastBlinnPhong(context);
+    //part 2
 }
