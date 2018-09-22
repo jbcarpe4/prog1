c907ffae857b76cc179beffac2172eb7ebcfcc6a
Joshua Carpenter <joshua1575@gmail.com>
2018-09-21 23:14:07 -0400

    Update drawstuff.js

--- drawstuff.js
+++ drawstuff.js
@@ -479,9 +479,7 @@ function rayCastBlinnPhong(context) {
                             var lVect = new Vector(-3 * w, w, -0.5 * w);
                             lVect = Vector.subtract(lVect,nVect);
                             lVect = Vector.normalize(lVect);
-                            
-                            nVect = Vector.normalize(nVect);
-                            
+                                                        
                             var NdotL = Vector.dot(lVect,nVect); // rect in xy plane
 
                             var difColor = new Color();
